8a17b9eb85cb59520b6029b4c835c1de05a9f436
Stephen Haberman <shaberman@homebound.com>
2020-07-28 23:35:13 -0500

    Make newResponse smart enough to use the right data factory.

---
+++ integration/integration.test.ts
@@ -0,0 +1,59 @@
+import { newAuthorSummary, newGetAuthorSummariesResponse, newSaveAuthorResponse } from "./graphql-types";
+
+describe("factories", () => {
+  it("sets the type name for mutation results", () => {
+    const { result } = newSaveAuthorResponse({ input: { name: "a" } }, { saveAuthor: { author: { name: "a" } } });
+    expect(result.data).toMatchInlineSnapshot(`
+      Object {
+        "__typename": "Mutation",
+        "saveAuthor": Object {
+          "__typename": "SaveAuthorResult",
+          "author": Object {
+            "__typename": "Author",
+            "birthday": null,
+            "name": "a",
+            "popularity": "Low",
+            "summary": Object {
+              "__typename": "AuthorSummary",
+              "amountOfSales": null,
+              "author": [Circular],
+              "numberOfBooks": 0,
+            },
+            "working": null,
+          },
+        },
+      }
+    `);
+  });
+
+  it("sets the type name for query results", () => {
+    const { result } = newGetAuthorSummariesResponse({
+      authorSummaries: [newAuthorSummary()],
+    });
+    expect(result.data).toMatchInlineSnapshot(`
+      Object {
+        "__typename": "Query",
+        "authorSummaries": Array [
+          Object {
+            "__typename": "AuthorSummary",
+            "amountOfSales": null,
+            "author": Object {
+              "__typename": "Author",
+              "birthday": null,
+              "name": "name",
+              "popularity": "Low",
+              "summary": Object {
+                "__typename": "AuthorSummary",
+                "amountOfSales": null,
+                "author": [Circular],
+                "numberOfBooks": 0,
+              },
+              "working": null,
+            },
+            "numberOfBooks": 0,
+          },
+        ],
+      }
+    `);
+  });
+});
