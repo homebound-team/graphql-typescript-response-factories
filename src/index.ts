74c4d7bf566a4b0ae7fc64596dbfdd9200691d0f
Stephen Haberman <shaberman@homebound.com>
2020-07-29 10:43:36 -0500

    Format.

--- src/index.ts
+++ src/index.ts
@@ -13,9 +13,9 @@ import PluginOutput = Types.PluginOutput;
 /** Generates `newQueryResponse({ ... })` factory functions in our `graphql-types` codegen output. */
 export const plugin: PluginFunction = async (schema, documents) => {
   const factories: Code[] = [];
-  documents.forEach(d => {
+  documents.forEach((d) => {
     if (d.document) {
-      d.document.definitions.forEach(d => {
+      d.document.definitions.forEach((d) => {
         if (d.kind === "OperationDefinition" && d.name) {
           factories.push(newOperationFactory(schema, d));
         }
@@ -34,7 +34,7 @@ function newOperationFactory(schema: GraphQLSchema, def: OperationDefinitionNode
 
   return code`
     interface ${name}DataOptions {
-        ${def.selectionSet.selections.map(s => {
+        ${def.selectionSet.selections.map((s) => {
           if (s.kind === "Field") {
             const name = s.name.value;
             const field = rootType?.getFields()[name];
@@ -55,7 +55,7 @@ function newOperationFactory(schema: GraphQLSchema, def: OperationDefinitionNode
     export function new${name}Data(data: ${name}DataOptions) {
       return {
         __typename: "${operation}" as const,
-        ${def.selectionSet.selections.map(s => {
+        ${def.selectionSet.selections.map((s) => {
           // This is the top-level Mutation/Query result, so usually/basically always has a single
           // field like `saveAuthor: AuthorResult!` where we can use the existing `newAuthorResult` factory.
           if (s.kind === "Field") {
